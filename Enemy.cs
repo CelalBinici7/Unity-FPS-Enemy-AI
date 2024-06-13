43fa46771478b0f2fdb0202aa0039288d8212618
Celal Binici <celalbinici150@gmail.com>
2024-06-13 20:18:14 +0300

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -14,16 +14,60 @@ public class Enemy : MonoBehaviour
     private string currentState;
 
     public Path path;
+
+      private GameObject player;
+
+  public GameObject Player { get => player; }
+ 
+  [Header("Sight values")]
+  public float sightDistance = 20f;
+  public float eyeHeight;
+  public float fieldOfView = 85f;
+
+  [Header("Weapon Values")]
+  public Transform gunBarrel;//add an empty object to the barrel of the gun
+  [Range(0.1f, 10f)]
+  public float fireRate;
+
     void Start()
     {
         stateMachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         stateMachine.Initialise();
+         player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+        void Update()
     {
-        
+        CanSeePlayer();
+        currentState = stateMachine.activeState.ToString();
     }
+
+    public bool CanSeePlayer()
+    {
+        if (player!=null)
+        {
+            // is the player close enough to seen
+            if (Vector3.Distance(transform.position,player.transform.position) <sightDistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position -(Vector3.up*eyeHeight);
+                float angleToPlayer = Vector3.Angle(targetDirection,transform.forward);
+                if (angleToPlayer >=-fieldOfView && angleToPlayer<=fieldOfView)
+                {
+                    Ray ray = new Ray(transform.position+(Vector3.up*eyeHeight), targetDirection);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray,out hit,sightDistance))
+                    {
+                        if (hit.transform.gameObject==player)
+                        {
+                            return true;
+                        }
+                    }
+                    Debug.DrawRay(ray.origin, ray.direction * sightDistance);
+                }
+            }
+        }
+        return false;
+    }
+}
 }
